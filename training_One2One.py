63b69dc366983c4b2c702df101d9197b99d6a583
mshamsi <meysam.shamsi@univ-lemans.fr>
2025-11-19 17:33:53 +0100

    epoch to 10

--- training_One2One.py
+++ training_One2One.py
@@ -45,8 +45,8 @@ devSet = DataLoader(ds_dev, batch_size=dataset_conf['batch_size'])
 conf={}
 conf["board_size"]=BOARD_SIZE
 conf["path_save"]="save_models"
-conf['epoch']=50
-conf["earlyStopping"]=5
+conf['epoch']=10
+conf["earlyStopping"]=2
 conf["len_inpout_seq"]=len_samples
 conf["LSTM_conf"]={}
 conf["LSTM_conf"]["hidden_dim"]=128
